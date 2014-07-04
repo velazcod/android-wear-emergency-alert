bc333a2bbf161faf0eb46b9779db3aa255ae8dad
Daniel Velazco <velazcod@gmail.com>
2014-07-03 19:22:32 -0700

    Add APACHE license

--- mobile/src/main/java/com/danvelazco/android/wear/emergencyalert/service/WearAlertService.java
+++ mobile/src/main/java/com/danvelazco/android/wear/emergencyalert/service/WearAlertService.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2014 Daniel Velazco
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.danvelazco.android.wear.emergencyalert.service;
 
 import android.accessibilityservice.AccessibilityService;
@@ -18,7 +34,6 @@ import android.support.v4.app.NotificationCompat;
 import android.telephony.SmsManager;
 import android.text.TextUtils;
 import android.text.format.DateUtils;
-import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 import android.widget.Toast;
 import com.danvelazco.android.wear.emergencyalert.AlertPreferencesActivity;
--- wear/src/main/java/com/danvelazco/android/wear/emergencyalert/AlertTriggerActivity.java
+++ wear/src/main/java/com/danvelazco/android/wear/emergencyalert/AlertTriggerActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2014 Daniel Velazco
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.danvelazco.android.wear.emergencyalert;
 
 import android.app.Activity;
@@ -23,6 +39,12 @@ import java.util.HashSet;
 
 public class AlertTriggerActivity extends Activity implements GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener {
+
+    /**
+     * TODO:
+     * - Detect a crash and automatically start the emergency alert on the watch with a timeout
+     * http://stackoverflow.com/questions/6291931/how-to-calculate-g-force-using-x-y-z-values-from-the-accelerometer-in-android
+     */
 
     // Constants
     private static final String TAG = "AlertTriggerActivity";
